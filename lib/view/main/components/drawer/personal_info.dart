f79a10b459eb9f4fbfb6a095f76298330647bd0d
srisivasA <srisivas.a@technotackle.com>
2025-02-21 15:24:23 +0530

    portfolio

---
+++ lib/view/main/components/drawer/personal_info.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../../res/constants.dart';
+import 'header_info.dart';
+
+class PersonalInfo extends StatelessWidget {
+  const PersonalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: defaultPadding/2,),
+        AreaInfoText(title: 'Contact', text: '9124227606'),
+        AreaInfoText(title: 'Email', text: 'srisivas362@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@Srisivas A'),
+        // AreaInfoText(title: 'Github', text: '@hamad-anwar'),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Text('Skills',style: TextStyle(color: Colors.white),),
+        SizedBox(
+          height: defaultPadding,
+        ),
+      ],
+    );
+  }
+}
